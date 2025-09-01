45096e0ba043e98f078852b931a9e37870aa571c
rodrigo-olaya <rom.fitz@gmail.com>
2025-08-31 21:21:18 -0400

    Thermistor connection working, receiving correct temperatures

--- smart-fridge/main/hello_world_main.c
+++ smart-fridge/main/hello_world_main.c
@@ -15,56 +15,50 @@
 #include "onewire.h"
 #include "ds18x20.h"
 #include <inttypes.h>
+#include <portmacro.h>
+#include "esp_mac.h"
 
-#define GPIO_NUM 13
+#define GPIO_NUM 4
 
 void app_main(void)
 {
-    printf("Hello world!\n");
-
     float temperature;
 
     onewire_addr_t addr_list[4];
     size_t found = 0;
 
-    ds18x20_scan_devices(GPIO_NUM, addr_list, sizeof(addr_list), &found);
+    esp_err_t ret = ds18x20_scan_devices(GPIO_NUM, addr_list, sizeof(addr_list), &found);
 
-    printf("%" PRIx64 "\n", addr_list[0]);
+    if (ret != ESP_OK) {
+        printf("Error finding devices: %X\n", ret);
+        return;
+    }
+    else {
+        uint8_t *addr_bytes = (uint8_t*)&addr_list[0];
+        printf("Address: ");
+        for (int i = 0; i < 8; i++) {
+            printf("%02X", addr_bytes[i]);
+        }
+        printf("\n");
+    }
 
-    ds18b20_read_temperature(GPIO_NUM, addr_list[0], &temperature);
+    ret = ds18x20_measure(GPIO_NUM, addr_list[0], true);
+    if (ret != ESP_OK) {
+        printf("Scan failed: 0x%X (%s)\n", ret, esp_err_to_name(ret));
+        return;
+    }
 
-    printf("Temperature: %.2f\n", temperature);
+    ret = ds18b20_read_temperature(GPIO_NUM, addr_list[0], &temperature);
+    if (ret != ESP_OK) {
+        printf("Error reading temp: %X\n", ret);
+        return;
+    }
+    else {
+        printf("Temperature: %.2f\n", temperature);
+    }
 
-    // /* Print chip information */
-    // esp_chip_info_t chip_info;
-    // uint32_t flash_size;
-    // esp_chip_info(&chip_info);
-    // printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-    //        CONFIG_IDF_TARGET,
-    //        chip_info.cores,
-    //        (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-    //        (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-    //        (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-    //        (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+    vTaskDelay(500 / portTICK_PERIOD_MS);
 
-    // unsigned major_rev = chip_info.revision / 100;
-    // unsigned minor_rev = chip_info.revision % 100;
-    // printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    // if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-    //     printf("Get flash size failed");
-    //     return;
-    // }
-
-    // printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-    //        (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    // printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    // for (int i = 10; i >= 0; i--) {
-    //     printf("Restarting in %d seconds...\n", i);
-    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    // }
-    // printf("Restarting now.\n");
     fflush(stdout);
     esp_restart();
 }
