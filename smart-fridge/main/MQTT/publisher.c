272d35b4e61f9cc0103145363dd885212f650a73
rodrigo-olaya <rom.fitz@gmail.com>
2025-09-18 22:07:30 -0400

    Update mosquitto code, not tested

--- smart-fridge/main/MQTT/publisher.c
+++ smart-fridge/main/MQTT/publisher.c
@@ -3,21 +3,21 @@
 void publish_data() {
     mosquitto_lib_init();
 
-    const char id = "mqtt_client";
+    const char* id = "mqtt_client";
 
-    struct mosquitto *mosquitto_ret = mosquitto_new(&id, true, NULL);
+    struct mosquitto *mosquitto_ret = mosquitto_new(id, true, NULL);
     if (mosquitto_ret == NULL) {
         // clean up if mosquitto client instance creation fails
         mosquitto_lib_cleanup();
     }
 
-    mosquitto_connect();
+    int mosquitto_connection = mosquitto_connect(mosquitto_ret, MQTT_BROKER_IP, MQTT_PORT, 60);
 
-    mosquitto_publish();
+    int publish_ret = mosquitto_publish(mosquitto_ret, NULL, "home/esp32/livingroom/temp", strlen(MQTT_PAYLOAD), MQTT_PAYLOAD, 0, false);
 
-    mosquitto_disconnect();
+    mosquitto_disconnect(mosquitto_ret);
 
-    mosquitto_destroy();
+    mosquitto_destroy(mosquitto_ret);
 
     mosquitto_lib_cleanup();
 }
--- smart-fridge/main/MQTT/publisher.h
+++ smart-fridge/main/MQTT/publisher.h
@@ -1,3 +1,7 @@
 #include "mosquitto.h"
+#include "../config/private_config.h"
+#include "string.h"
+
+#define MQTT_PAYLOAD "23.5"
 
 void publish_data();
