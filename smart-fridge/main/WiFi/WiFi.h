56cb82719c8a0c8c87d523306d5a9b54340ac622
rodrigo-olaya <rom.fitz@gmail.com>
2025-09-25 21:22:53 -0400

    Move WiFi to designated space

---
+++ smart-fridge/main/WiFi/WiFi.h
@@ -0,0 +1,7 @@
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "../config/private_config.h"
+
+void WiFi_init();
--- smart-fridge/main/main.c
+++ smart-fridge/main/main.c
@@ -19,41 +19,16 @@
 #include "sensors/thermistor.h"
 #include "control/control.h"
 #include "communication/queues.h"
+#include "WiFi/WiFi.h"
 #include "esp_system.h"
-#include "nvs_flash.h"
-#include "esp_event.h"
-#include "esp_netif.h"
-#include "esp_wifi.h"
-#include "../config/private_config.h"
 #include "esp_log.h"
 
 extern QueueHandle_t xSensorDataQueue;
 
 void app_main(void)
 {
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = WIFI_SSID,
-            .password = WIFI_PWD,
-            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_ERROR_CHECK(esp_wifi_connect());
-
-    vTaskDelay(pdMS_TO_TICKS(6000));
+    /* Initialize WiFi */
+    WiFi_init();
 
     /* Initialize the queues */
     queues_init();
