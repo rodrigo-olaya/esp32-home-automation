c0fb83a6786210fb3f648f1a4d5d242d60d83544
rodrigo-olaya <rom.fitz@gmail.com>
2025-10-06 22:17:22 -0400

    Add comments and cleanup control logic

--- smart-fridge/main/control/control.h
+++ smart-fridge/main/control/control.h
@@ -3,12 +3,15 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
 #include "../MQTT/publisher.h"
 #include "../sensors/sensor_defines.h"
 
 extern QueueHandle_t xSensorDataQueue;
 
+/**
+ * @brief This function reads the temperature from the DS18B20 and calls the sensor control function with the function.
+ * @param sensor_data
+ */
 void sensor_send_data(sensorData_t *sensor_data);
 
 void vReceiverTask( void *pvParameters );
