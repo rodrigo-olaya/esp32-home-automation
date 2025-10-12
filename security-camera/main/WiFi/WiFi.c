613bc363d6cba20e57b5eb6d47e31a4576cdaa30
rodrigo-olaya <rom.fitz@gmail.com>
2025-10-12 15:27:32 -0400

    Add WiFi initialization

---
+++ security-camera/main/WiFi/WiFi.c
@@ -0,0 +1,30 @@
+#include "WiFi.h"
+
+void WiFi_init() {
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PWD,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_ERROR_CHECK(esp_wifi_connect());
+
+    vTaskDelay(pdMS_TO_TICKS(6000));
+}    
+    
+    
+    
--- security-camera/main/security-camera.c
+++ security-camera/main/security-camera.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include <inttypes.h>
+#include <portmacro.h>
+#include "esp_mac.h"
+#include "WiFi/WiFi.h"
+#include "esp_system.h"
+#include "esp_log.h"
 
 void app_main(void)
 {
-
+    WiFi_init();
 }
