4c94dd72b1d99b11346fc1d29ac2a569db5c4769
rodrigo-olaya <rom.fitz@gmail.com>
2025-10-22 19:40:14 -0400

    Cleanup before merge

--- security-camera/main/camera/camera.h
+++ security-camera/main/camera/camera.h
@@ -14,10 +14,10 @@
 #define CAM_PIN_D6      34
 #define CAM_PIN_D5      39
 #define CAM_PIN_D4      36
-#define CAM_PIN_D3       21
-#define CAM_PIN_D2       19 
-#define CAM_PIN_D1       18 
-#define CAM_PIN_D0        5 
+#define CAM_PIN_D3      21
+#define CAM_PIN_D2      19 
+#define CAM_PIN_D1      18 
+#define CAM_PIN_D0      5 
 #define CAM_PIN_VSYNC   25
 #define CAM_PIN_HREF    23
 #define CAM_PIN_PCLK    22
