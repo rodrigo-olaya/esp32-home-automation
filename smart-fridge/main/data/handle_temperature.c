02f5907a14f3986573a34c9965f5e86e71054422
rodrigo-olaya <rom.fitz@gmail.com>
2025-10-31 22:52:02 -0400

    Add temp data handling with TDD

---
+++ smart-fridge/main/data/handle_temperature.c
@@ -0,0 +1,5 @@
+#include "handle_temperature.h"
+
+double convert_to_farenheit(double celsius) {
+    return 0.0;
+}
---
+++ smart-fridge/main/data/handle_temperature.h
@@ -0,0 +1,14 @@
+#ifndef HANDLE_TEMPERATURE_H
+#define HANDLE_TEMPERATURE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double convert_to_farenheit(double celsius);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
