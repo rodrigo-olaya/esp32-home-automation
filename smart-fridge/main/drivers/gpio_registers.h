6ffcf9b70f14d0d81ef40d920fff9ce363ee5e57
rodrigo-olaya <rom.fitz@gmail.com>
2025-11-03 21:00:11 -0500

    Add GPIO addresses

---
+++ smart-fridge/main/drivers/gpio_registers.h
@@ -0,0 +1,10 @@
+#ifndef GPIO_REGISTERS_H
+#define GPIO_REGISTERS_H
+
+#define GPIO_BASE           0x3ff44000
+#define GPIO_OUT_REG        (*(volatile uint32_t*)(GPIO_BASE + 0x0004))
+#define GPIO_OUT_W1TS_REG   (*(volatile uint32_t*)(GPIO_BASE + 0x0008))
+#define GPIO_OUT_W1TC_REG   (*(volatile uint32_t*)(GPIO_BASE + 0x000C))
+#define GPIO_ENABLE_REG     (*(volatile uint32_t*)(GPIO_BASE + 0x0020))
+
+#endif //GPIO_REGISTERS_H
