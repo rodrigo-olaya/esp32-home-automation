2f60e194593b5957c7efbb9e8f530044e25c785a
rodrigo-olaya <rom.fitz@gmail.com>
2025-11-03 21:26:21 -0500

    implement gpio driver api

--- smart-fridge/main/drivers/gpio_driver.c
+++ smart-fridge/main/drivers/gpio_driver.c
@@ -1,2 +1,16 @@
 #include "gpio_driver.h"
 #include "gpio_registers.h"
+
+void gpio_set_direction(gpio_num_t gpio_pin, gpio_direction_t direction) {
+    if (direction == GPIO_OUTPUT) {
+        GPIO_ENABLE_REG = (1 << gpio_pin);
+    }
+}
+
+void gpio_set_high(gpio_num_t gpio_pin) {
+    GPIO_OUT_W1TS_REG = (1 << gpio_pin);
+}
+
+void gpio_set_low(gpio_num_t gpio_pin) {
+    GPIO_OUT_W1TC_REG = (1 << gpio_pin);
+}
