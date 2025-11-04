301751f39f9f5e184ec8ca095911913a6c07605d
rodrigo-olaya <rom.fitz@gmail.com>
2025-11-03 21:32:05 -0500

    update imports

--- smart-fridge/main/drivers/gpio_registers.h
+++ smart-fridge/main/drivers/gpio_registers.h
@@ -1,6 +1,8 @@
 #ifndef GPIO_REGISTERS_H
 #define GPIO_REGISTERS_H
 
+#include <stdint.h>
+
 #define GPIO_BASE           0x3ff44000
 #define GPIO_OUT_REG        (*(volatile uint32_t*)(GPIO_BASE + 0x0004))
 #define GPIO_OUT_W1TS_REG   (*(volatile uint32_t*)(GPIO_BASE + 0x0008))
