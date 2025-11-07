5017cc90fa6b149756427238de98733280cd3270
rodrigo-olaya <rom.fitz@gmail.com>
2025-11-06 21:29:19 -0500

    use gpio_number_t for step and dir

--- smart-fridge/main/motors/nema17.c
+++ smart-fridge/main/motors/nema17.c
@@ -8,24 +8,27 @@ void set_motor_speed(uint8_t target_delay_in_msec){
     step_delay_in_msec = target_delay_in_msec;
 }
 
-void step() {
-    gpio_set_high(GPIO_NUMBER_5);
+void step(gpio_number_t motor_step_gpio) {
+    gpio_set_high(motor_step_gpio);
     vTaskDelay(step_delay_in_msec / portTICK_PERIOD_MS);
-    gpio_set_low(GPIO_NUMBER_5);
+    gpio_set_low(motor_step_gpio);
     vTaskDelay(step_delay_in_msec / portTICK_PERIOD_MS);
 }
 
 void move_to_angle(float target_angle) {}
 
 void drive_motor(){
-    gpio_set_dir(GPIO_NUMBER_5, GPIO_OUTPUT);  // STEP
-    gpio_set_dir(GPIO_NUMBER_18, GPIO_OUTPUT); // DIR
+    gpio_number_t motor_step_gpio = GPIO_NUMBER_5;
+    gpio_number_t motor_dir_gpio = GPIO_NUMBER_18;
 
-    gpio_set_low(GPIO_NUMBER_18);  // Set direction
+    gpio_set_dir(motor_step_gpio, GPIO_OUTPUT);  // STEP
+    gpio_set_dir(motor_dir_gpio, GPIO_OUTPUT); // DIR
+
+    gpio_set_low(motor_dir_gpio);  // Set direction
 
     set_motor_speed(25);
 
     while(1){
-        step();
+        step(motor_step_gpio);
     }    
 }
