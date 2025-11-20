f3486fb92f9be67b833eec8937d9980d831d82dc
rodrigo-olaya <rom.fitz@gmail.com>
2025-11-19 21:58:42 -0500

    More comments

--- smart-fridge/main/motors/nema17.h
+++ smart-fridge/main/motors/nema17.h
@@ -15,16 +15,40 @@ typedef enum {
     COUNTERCLOCKWISE = 1
 } motor_direction_t;
 
+/**
+ * @brief Configures GPIO direction pin according to command
+ * @param event MQTT event that carries what the motor needs to do
+ */
 void configure_motor(esp_mqtt_event_handle_t event);
 
+/**
+ * @brief 
+ * @param 
+ */
 void set_step_resolution();
 
+/**
+ * @brief 
+ * @param 
+ */
 void set_motor_speed(uint8_t step_time_in_msec);
 
+/**
+ * @brief 
+ * @param 
+ */
 void set_direction(motor_direction_t direction);
 
+/**
+ * @brief 
+ * @param 
+ */
 void step();
 
+/**
+ * @brief 
+ * @param 
+ */
 void move_to_angle(float target_angle);
 
 #endif
