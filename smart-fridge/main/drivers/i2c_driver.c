0f590d8250ae1df959b2cb424926c451c23444bc
rodrigo-olaya <rom.fitz@gmail.com>
2025-11-23 18:54:03 -0500

    functions for I2C start, stop and init

--- smart-fridge/main/drivers/i2c_driver.c
+++ smart-fridge/main/drivers/i2c_driver.c
@@ -1,7 +1,33 @@
 #include "i2c_driver.h"
 
-void i2c_init(){}
-void i2c_start(){}
-void i2c_stop(){}
+void i2c_init(){
+    gpio_set_dir(I2C_SCL_GPIO, GPIO_INPUT);
+    gpio_set_dir(I2C_SDA_GPIO, GPIO_INPUT);
+    ets_delay_us(10);
+}
+void i2c_start(){
+    gpio_set_dir(I2C_SCL_GPIO, GPIO_INPUT);
+    gpio_set_dir(I2C_SDA_GPIO, GPIO_INPUT);
+    ets_delay_us(10);
+
+    gpio_set_dir(I2C_SDA_GPIO, GPIO_OUTPUT);
+    gpio_set_low(I2C_SDA_GPIO);
+    ets_delay_us(10);
+
+    gpio_set_dir(I2C_SCL_GPIO, GPIO_OUTPUT);
+    gpio_set_low(I2C_SCL_GPIO);
+    ets_delay_us(10);
+}
+void i2c_stop(){
+    gpio_set_dir(I2C_SDA_GPIO, GPIO_OUTPUT);
+    gpio_set_low(I2C_SDA_GPIO);
+    ets_delay_us(10);
+
+    gpio_set_dir(I2C_SCL_GPIO, GPIO_INPUT);
+    ets_delay_us(10);
+    
+    gpio_set_dir(I2C_SDA_GPIO, GPIO_INPUT);
+    ets_delay_us(10);
+}
 i2c_return_t i2c_write_byte(uint8_t data){}
 uint8_t i2c_read_byte(bool send_ack){}
