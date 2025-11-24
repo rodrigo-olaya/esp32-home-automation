c49ef848f055f08b72f1f8f8397668d174897f39
rodrigo-olaya <rom.fitz@gmail.com>
2025-11-23 19:18:36 -0500

    Add write logic for I2C driver

--- smart-fridge/main/drivers/i2c_driver.c
+++ smart-fridge/main/drivers/i2c_driver.c
@@ -25,9 +25,42 @@ void i2c_stop(){
 
     gpio_set_dir(I2C_SCL_GPIO, GPIO_INPUT);
     ets_delay_us(10);
-    
+
     gpio_set_dir(I2C_SDA_GPIO, GPIO_INPUT);
     ets_delay_us(10);
 }
-i2c_return_t i2c_write_byte(uint8_t data){}
-uint8_t i2c_read_byte(bool send_ack){}
+i2c_return_t i2c_write_byte(uint8_t data){
+
+    for (int i = 0; i < 8; i++){
+        uint8_t bit = (data >> (7 - i)) & 0x01;
+        if (bit == HIGH) {
+            gpio_set_dir(I2C_SDA_GPIO, GPIO_INPUT);
+        }
+        else if (bit == LOW) {
+            gpio_set_dir(I2C_SDA_GPIO, GPIO_OUTPUT);
+            gpio_set_low(I2C_SDA_GPIO);
+        }
+        ets_delay_us(I2C_DELAY_US);
+
+        gpio_set_dir(I2C_SCL_GPIO, GPIO_INPUT);
+        ets_delay_us(I2C_DELAY_US);
+
+        gpio_set_dir(I2C_SCL_GPIO, GPIO_OUTPUT);
+        gpio_set_low(I2C_SCL_GPIO);
+        ets_delay_us(10);
+    }
+    gpio_set_dir(I2C_SCL_GPIO, GPIO_INPUT);
+    ets_delay_us(10);
+
+    gpio_set_dir(I2C_SDA_GPIO, GPIO_INPUT);
+    ets_delay_us(10);
+    i2c_return_t ack = gpio_read(I2C_SDA_GPIO);
+    gpio_set_dir(I2C_SCL_GPIO, GPIO_OUTPUT);
+    gpio_set_low(I2C_SCL_GPIO);
+    ets_delay_us(I2C_DELAY_US);
+    
+    return ack;
+}
+uint8_t i2c_read_byte(bool send_ack){
+    return 0;
+}
