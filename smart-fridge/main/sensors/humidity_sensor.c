485305bfd8ffe5b10e8b4216cebfda3f0c1f46c9
rodrigo-olaya <rom.fitz@gmail.com>
2025-11-24 21:25:38 -0500

    Cleanup

--- smart-fridge/main/sensors/humidity_sensor.c
+++ smart-fridge/main/sensors/humidity_sensor.c
@@ -4,19 +4,13 @@ static const char *HUMIDITY_TAG = "SHT3x";
 uint8_t sht3x_addr = 0x44;
 
 void humidity_sensor_initialize() {
-    // uint8_t sht3x_addr = 0x44;
-
     i2c_init();
-    // i2c_start();
-    // i2c_return_t ack = i2c_write_byte(sht3x_addr << 1);
-    // i2c_stop();
-    // ESP_LOGI(HUMIDITY_TAG, "ACK: %d", ack);
 }
 
 void read_humidity_new() {
 
     i2c_start();
-    i2c_return_t ack1 = i2c_write_byte(0x44 << 1);
+    i2c_return_t ack1 = i2c_write_byte(sht3x_addr << 1);
     ESP_LOGI(HUMIDITY_TAG, "ACK: %d", ack1);
     i2c_return_t ack2 = i2c_write_byte(0x2C);
     ESP_LOGI(HUMIDITY_TAG, "ACK: %d", ack2);
@@ -27,7 +21,7 @@ void read_humidity_new() {
     vTaskDelay(pdMS_TO_TICKS(20));
 
     i2c_start();
-    i2c_write_byte((0x44 << 1) | 1);
+    i2c_write_byte((sht3x_addr << 1) | 1);
     uint8_t temp_msb = i2c_read_byte(true);
     uint8_t temp_lsb = i2c_read_byte(true);
     uint8_t temp_crc = i2c_read_byte(true);
