938c82f12258e2ff2498de54f06e439a6462e2c3
rodrigo-olaya <rom.fitz@gmail.com>
2025-11-25 21:35:20 -0500

    Flip inputs in tdd

--- smart-fridge/test/humidity_conversion_tdd.cpp
+++ smart-fridge/test/humidity_conversion_tdd.cpp
@@ -12,9 +12,9 @@ TEST_GROUP(HumidityConversionGroup)
 TEST(HumidityConversionGroup, BytesToHumidity)
 {
     DOUBLES_EQUAL(57.7, bytes_to_humidity(147, 194), 0.01);
-    DOUBLES_EQUAL(bytes_to_humidity(0, 0), 0.0, 0.01);
-    DOUBLES_EQUAL(bytes_to_humidity(128, 0), 50.0, 0.01);
-    DOUBLES_EQUAL(bytes_to_humidity(255,255), 100.0, 0.01);
-    DOUBLES_EQUAL(bytes_to_humidity(85,51), 33.3, 0.01);
-    DOUBLES_EQUAL(bytes_to_humidity(204,204), 80.0, 0.01);
+    DOUBLES_EQUAL(0.0, bytes_to_humidity(0, 0), 0.01);
+    DOUBLES_EQUAL(50.0, bytes_to_humidity(128, 0), 0.01);
+    DOUBLES_EQUAL(100.0, bytes_to_humidity(255,255), 0.01);
+    DOUBLES_EQUAL(33.3, bytes_to_humidity(85,51), 0.01);
+    DOUBLES_EQUAL(80.0, bytes_to_humidity(204,204), 0.01);
 };
