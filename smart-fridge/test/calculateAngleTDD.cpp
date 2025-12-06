9f41ab2a74a26d0cfad0f7bf69ef3f9ed72d35fc
rodrigo-olaya <rom.fitz@gmail.com>
2025-12-06 16:34:57 -0500

    More TDD

--- smart-fridge/test/calculateAngleTDD.cpp
+++ smart-fridge/test/calculateAngleTDD.cpp
@@ -11,7 +11,11 @@ TEST_GROUP(angleGroup)
 
 TEST(angleGroup, calculateAngle)
 {
-   DOUBLES_EQUAL(calculate_angle(4, 0), 90.0, 0.01);
-   DOUBLES_EQUAL(calculate_angle(0, 0), 0.0, 0.01);
-   DOUBLES_EQUAL(calculate_angle(8, 0), 180.0, 0.01);
+   DOUBLES_EQUAL(90.0, calculate_angle(4, 0), 0.1);
+   DOUBLES_EQUAL(0.0, calculate_angle(0, 0), 0.1);
+   DOUBLES_EQUAL(180.0, calculate_angle(8, 0), 0.1);
+   DOUBLES_EQUAL(270.0, calculate_angle(12, 0), 0.1);
+   DOUBLES_EQUAL(359.91, calculate_angle(15, 255), 0.1);
+   DOUBLES_EQUAL(154.7, calculate_angle(6, 224), 0.1);
+   DOUBLES_EQUAL(10.0, calculate_angle(0, 114), 0.1);
 }
